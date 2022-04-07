927e0103ff5a2037b14d2e94713e459e4b9963c7
Elie Andraos <elieandraos31@gmail.com>
2022-04-07 17:24:29 +0300

    :green_heart: node & browser bundling support

--- src/templates/vanilla/scripts/build.js
+++ src/templates/vanilla/scripts/build.js
@@ -1,23 +1,46 @@
 const esbuild = require('esbuild')
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
 
-esbuild
-    .build({
-        entryPoints: ['src/index.js'],
-        bundle: true,
-        minify: true,
-        logLevel: 'info',
-        platform: 'browser',
-        outfile: 'dist/index.js',
-        plugins: [
-            nodeExternalsPlugin({
-                dependencies: true,
-                devDependencies: false,
-                peerDependencies: false,
-                optionalDependencies: false,
-            }),
-        ],
-    })
-    .catch(() => {
-        process.exit(1)
-    })
+// ESM module (ECMAScript)
+// Meant for "import" statements in (modern browsers and bundlers)
+esbuild.build({
+    entryPoints: ['src/clockwork.js'],
+    outfile: 'dist/index.esm.js',
+    bundle: true,
+    minify: true,
+    format: 'esm',
+    target: ['esnext'],
+    plugins: [
+        nodeExternalsPlugin({
+            dependencies: true,
+            devDependencies: false,
+            peerDependencies: false,
+            optionalDependencies: false,
+        }),
+    ],
+    logLevel: 'info',
+}).catch( () => {
+    process.exit(1)
+})
+
+// CJS module (CommonJS)
+// Meant for "require" statements in (node)
+esbuild.build({
+    entryPoints: ['src/clockwork.js'],
+    outfile: 'dist/index.cjs.js',
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    target: ['node10.4'],
+    plugins: [
+        nodeExternalsPlugin({
+            dependencies: true,
+            devDependencies: false,
+            peerDependencies: false,
+            optionalDependencies: false,
+        }),
+    ],
+    logLevel: 'info',
+}).catch( () => {
+    process.exit(1)
+})
