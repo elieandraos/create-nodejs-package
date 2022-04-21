a9e0637c6da23094c183dd33b0e5c0be9d93b8aa
Elie Andraos <elieandraos31@gmail.com>
2022-04-21 15:49:18 +0300

    :bug: fix generated .gitgnore file

--- src/tasks/install/copyTemplate.js
+++ src/tasks/install/copyTemplate.js
@@ -1,9 +1,14 @@
 import chalk from 'chalk'
 import _ from '@elieandraos/cli-tools'
+import fs from 'fs'
 
 const copyTemplate = async (packageInfo) => {
     try {
         await _.copy(packageInfo.templateDir, packageInfo.root)
+
+        fs.rename(packageInfo.root + '/.gitignore.example', packageInfo.root + '/.gitignore', (err) => {
+            if(err) console.log(err)
+        })
 
         await _.respondOk(
             `Scaffolded package files and directories in ${chalk.cyan(
